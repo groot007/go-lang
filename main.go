3365816c1333d6e36d2a06511ca7ff8d8465e0d2
Mykola Stanislavchuk <kola.stani@gmail.com>
2024-12-26 19:57:30 +0100

    change main

--- main.go
+++ main.go
@@ -6,30 +6,17 @@ import (
 	"os"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/fortune":
-		handleFortune(w, r)
-	case "/fortune/category":
-		handleFortuneByCategory(w, r)
-	default:
-		http.NotFound(w, r)
-	}
-}
-
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
 
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
-	http.HandleFunc("/fortune", handleFortune)
-	http.HandleFunc("/fortune/category", handleFortuneByCategory)
+    http.HandleFunc("/fortune", handleFortune)
+    http.HandleFunc("/fortune/category", handleFortuneByCategory)
 
-	log.Println("HTTP server started on :" + port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatalf("ListenAndServe: %v", err)
-	}
+    log.Printf("Starting server on port %s", port)
+    if err := http.ListenAndServe(":"+port, nil); err != nil {
+        log.Fatal(err)
+    }
 }
